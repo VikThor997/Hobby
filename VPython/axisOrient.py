318397ce68b2a121423126ec1015455312076987
VikThor997 <viktorpuskar6@gmail.com>
2021-10-07 21:40:58 +0200

    Update axisOrient.py

--- VPython/axisOrient.py
+++ VPython/axisOrient.py
@@ -5,31 +5,35 @@ import numpy as np
 arrowL = 2
 arrowT = .02
 theta = 0
+bRadius = .1
 
 Xarrow = arrow(axis = vector(1,0,0), color = color.red, length = arrowL, shaftwidt = arrowT)
 Yarrow = arrow(axis = vector(0,1,0), color = color.green, length = arrowL, shaftwidt = arrowT)
 Zarrow = arrow(axis = vector(0,0,1), color = color.blue, length = arrowL, shaftwidt = arrowT)
 
-pntArrow = arrow(axis = vector(arrowL*np.cos(theta),arrowL*np.sin(theta),0), color = color.orange, length = arrowL, shaftwidt = arrowT)
+pntArrow =  arrow(axis = vector(1,0,0), color = color.orange, length = arrowL, shaftwidt = arrowT)
+
+myBall = sphere(make_trail = True ,radius = bRadius, color = color.red, pos = vector(arrowL,0,0))
 
 while True:
     for myAngle in np.linspace(0,2*np.pi,1000):
-        rate(200)
+        rate(150)
         pntArrow.axis = vector(arrowL*np.cos(myAngle),arrowL*np.sin(myAngle),0)
         pntArrow.length = arrowL
-    for myAngle in np.linspace(0,2*np.pi,1000):
-        rate(200)
+        myBall.pos = vector(arrowL*np.cos(myAngle),arrowL*np.sin(myAngle),0) 
+    for myAngle in np.linspace(0,5*np.pi/2,1000):
+        rate(150)
         pntArrow.axis = vector(arrowL*np.cos(myAngle),0,arrowL*np.sin(myAngle))
         pntArrow.length = arrowL
-    for myAngle in np.linspace(0,int(np.pi/2),1000):
-        rate(500)
-        pntArrow.axis = vector(arrowL*np.cos(myAngle),arrowL*np.sin(myAngle),0)
-        pntArrow.length = arrowL
+        myBall.pos = vector(arrowL*np.cos(myAngle),0,arrowL*np.sin(myAngle))
     for myAngle in np.linspace(0,2*np.pi,1000):
-        rate(200)
-        pntArrow.axis = vector(0,arrowL*np.cos(myAngle),arrowL*np.sin(myAngle))
+        rate(150)
+        pntArrow.axis = vector(0,arrowL*np.sin(myAngle),arrowL*np.cos(myAngle))
         pntArrow.length = arrowL
-    for myAngle in np.linspace(int(np.pi/2),0,1000):
-        rate(200)
-        pntArrow.axis = vector(arrowL*np.cos(myAngle),arrowL*np.sin(myAngle),0)
+        myBall.pos = vector(0,arrowL*np.sin(myAngle),arrowL*np.cos(myAngle))
+    for myAngle in np.linspace(np.pi/2,2*np.pi,1000):
+        rate(150)
+        pntArrow.axis = vector(arrowL*np.cos(myAngle),0,arrowL*np.sin(myAngle))
         pntArrow.length = arrowL
+        myBall.pos = vector(arrowL*np.cos(myAngle),0,arrowL*np.sin(myAngle))
+    
